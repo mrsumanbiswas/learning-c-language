948866597ac71a464bd4817120ec46724cb7dc4d
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-07-31 20:57:55 +0530

    Learning about comments in C language

---
+++ comments_in_c_language/comments_in_c_language.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+
+    single line comment
+
+    //  <--  those are used for single line comment
+
+    multiline comment
+
+    This entire block itself an example of multiline comment
+    it starts form `/` and `*` and ends with `*` and `/`
+
+    */
+    printf("Learning about Comments in C.\n");
+    return 0;
+}
