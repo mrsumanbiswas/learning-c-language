14dde8a4b45cac0d65c89507b78d262c3a29ba6b
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-07-31 20:49:50 +0530

    Hello World Program in C language

---
+++ first_program/hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\nThis is the first program\n"); // ``\n`` are used for creating new line
+    return 0;
+}
+
+/*
+To compile and run the code use:
+                                    ``gcc hello_world.c && ./a.out ``
+(using gcc as compiler)
+*/
