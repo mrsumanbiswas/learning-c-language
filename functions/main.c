9b645775bebb4b4a9be39327e8ba45659b499e3b
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-03 11:33:37 +0530

    varius type of standerd function

--- functions/main.c
+++ functions/main.c
@@ -1,4 +1,14 @@
-#include <stdio.h>
+#include <assert.h> //	Program assertion functions
+#include <ctype.h>  //	Character type functions
+#include <locale.h> //	Localization functions
+#include <math.h>   //	Mathematics functions
+#include <setjmp.h> //	Jump functions
+#include <signal.h> //	Signal handling functions
+#include <stdarg.h> //	Variable arguments handling functions
+#include <stdio.h>  //	Standard Input/Output functions
+#include <stdlib.h> //	Standard Utility functions
+#include <string.h> //	String handling functions
+#include <time.h>   //  Date time functions
 
 int main()
 {
